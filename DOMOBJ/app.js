ad2d682e803e9ffa8cd240d7d56f890307ab772c
Yeşim Gerdan <59263552+yesimgrdn@users.noreply.github.com>
2022-11-20 19:45:50 +0300

    Add files via upload

---
+++ DOMOBJ/app.js
@@ -0,0 +1,14 @@
+//window object 
+//window.document
+let value;
+value=document;
+value=document.all;
+value=document.all.length
+value=document.all[0];
+value=document.all[6];
+value=document.all(document.all.length-1);
+value=document.all[8];
+value=document.location.hostname;
+value=document.location.port
+value=document.location
+console.log(value);
